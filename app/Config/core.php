1bef3eda6574d4235c332d43c28ba1aa2401e1ef
owlman <jie.owl2008@gmail.com>
2014-07-27 15:03:12 +0800

    reboot config/core.php

--- app/Config/core.php
+++ app/Config/core.php
@@ -176,7 +176,7 @@
  * for instance. Each version can then have its own view cache namespace.
  * Note: The final cache file name will then be `prefix_cachefilename`.
  */
-	//Configure::write('Cache.viewPrefix', 'prefix');	
+	//Configure::write('Cache.viewPrefix', 'prefix');
 
 /**
  * Session configuration.
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'ADEFGVDHXNJKDXHHFCJZJDJMSHFCHHDHSHNSBNH');
+	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '18445454114212141221141415221432412211');
+	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
